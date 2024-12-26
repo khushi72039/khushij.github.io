e80fa80a886d6648c7e800ffb101698806a03933
khushi72039 <patelkhushi9687@gmail.com>
2024-12-26 22:17:47 +0530

    mywebsite

---
+++ get.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+ <h1>reading data from server</h1>
+ 
+ <form action="display.php" method="get">
+ User Name:<input type="text" name="uname"><br><br>
+ password :<input type="password" name="pass"><br><br>
+ <input type="submit" name="sub" value="khushi"><br><br>
+ </html>
+ </body>
+ 
